c7435f019f0e8c3c32c94127ef731079a3b489a5
Gabriel De Los Rios <gab.delosrios@gmail.com>
2023-11-05 21:01:04 -0300

    build: some extra config for cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,16 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    testIsolation: false,
+    baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+  },
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
     },
   },
 });
