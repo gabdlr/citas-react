5d0a8220d9cdb746c4d5ee1d39f42fb4f8893321
Gabriel De Los Rios <gab.delosrios@gmail.com>
2024-01-13 21:20:55 -0300

    fix: testing libraries

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import istanbul from "vite-plugin-istanbul";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    istanbul({
+      include: "src/*",
+      exclude: ["node_modules", "src/__tests__"],
+      extension: [".js", ".ts", ".vue"],
+      requireEnv: true,
+    }),
+  ],
   server: {
     host: true,
     port: 3000,
